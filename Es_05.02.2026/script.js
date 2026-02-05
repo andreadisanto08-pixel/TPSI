a4fc530a96e6ef7070a102635d6d399deb57d0d1
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2026-02-05 13:12:48 +0100

    agg

---
+++ Es_05.02.2026/script.js
@@ -0,0 +1,9 @@
+alert("sono un programma javascript moderno!");
+const pronto = confirm("Sei pronto a programmare?");
+const display = document.getElementById('pippo');
+if(pronto)
+{
+    display.textContent = 'ciao'
+}
+    
+
