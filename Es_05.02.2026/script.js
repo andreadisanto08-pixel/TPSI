657d87720816cac5bf92e7fa59aec2ff4d1c2669
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2026-03-14 22:31:13 +0100

    Delete script.js

--- Es_05.02.2026/script.js
+++
@@ -1,9 +0,0 @@
-alert("sono un programma javascript moderno!");
-const pronto = confirm("Sei pronto a programmare?");
-const display = document.getElementById('pippo');
-if(pronto)
-{
-    display.textContent = 'ciao'
-}
-    
-
